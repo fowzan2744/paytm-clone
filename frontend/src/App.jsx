0bc4ac58f24247a005167a1613048b22806251d3
Fowzan <153728662+fowzan2744@users.noreply.github.com>
2025-05-27 16:08:42 +0530

    package

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     <div>
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Signup />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/dashboard" element={<Dashboard />} />
